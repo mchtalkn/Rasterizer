6f1e293d91f49269cc1e12fb8004a89d14c2f475
mchtalkn <mucahit.a.yorganci@gmail.com>
2022-01-04 14:53:25 +0300

    line primitives

--- the2/GeneratedMesh.cpp
+++ the2/GeneratedMesh.cpp
@@ -4,12 +4,27 @@
 
 GeneratedMesh::GeneratedMesh(Mesh& original_, Scene& s):original(original_)
 {
-	for (Triangle& t : original.triangles) generated_triangles.push_back(generated_triangle(t,s));
+	if(original.type == 1)
+		for (Triangle& t : original.triangles) 
+			generated_triangles.push_back(generated_triangle(t,s));
+	else if(original.type == 1)
+		for (Triangle& t : original.triangles) {
+			generated_lines.push_back(generated_line(*s.vertices[t.vertexIds[0]], *s.vertices[t.vertexIds[1]]));
+			generated_lines.push_back(generated_line(*s.vertices[t.vertexIds[1]], *s.vertices[t.vertexIds[2]]));
+			generated_lines.push_back(generated_line(*s.vertices[t.vertexIds[2]], *s.vertices[t.vertexIds[0]]));
+		}
 }
+
 
 generated_triangle::generated_triangle(Triangle& t, Scene& s)
 {
 	vertices[0] = *s.vertices[t.vertexIds[0]];
 	vertices[1] = *s.vertices[t.vertexIds[1]];
 	vertices[2] = *s.vertices[t.vertexIds[2]];
+}
+
+generated_line::generated_line(const Vec4& v1, const Vec4& v2)
+{
+	vertices[0] = v1;
+	vertices[1] = v2;
 }
